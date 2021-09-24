0abacc7e41a9cdf51b6533ed31e3e35c0bc35ca6
nathanvo83 <nathanvo83@gmail.com>
2021-09-24 12:33:57 +1200

    init

---
+++ app/src/main/java/com/example/student_lib02/Test01.java
@@ -0,0 +1,10 @@
+package com.example.student_lib02;
+
+import com.example.lib02.Student;
+
+public class Test01 {
+    public void A() {
+        Student x = new Student();
+        x.name = "XXX";
+    }
+}
---
+++ lib02/src/main/java/com/example/lib02/Student.java
@@ -0,0 +1,6 @@
+package com.example.lib02;
+
+public class Student {
+    public String name;
+    public int age;
+}
